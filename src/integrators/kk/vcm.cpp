56b9b0aa986934c1a48732bb865026211de505c3
Kacper Madej <kacper.madej@u2i.com>
2014-12-11 12:23:23 +0100

    Draft implementation of building KDTree

--- src/integrators/kk/vcm.cpp
+++ src/integrators/kk/vcm.cpp
@@ -10,6 +10,16 @@
 MTS_NAMESPACE_BEGIN
 
 class VCMIntegrator : public Integrator {
+    typedef PointKDTree<PathVertexPtr> VCMKDTree;
+    typedef VCMKDTree::IndexType IndexType;
+    typedef VCMKDTree::SearchResult SearchResult;
+
+    enum {
+	EdVCMData = 0,
+	edVCData = 1,
+	edVMData = 2
+    };
+
 public:
 	VCMIntegrator(const Properties &props) : Integrator(props) {
 		/* Load the parameters / defaults */
@@ -73,6 +83,8 @@ public:
 
 		std::vector<Path* > paths;
 		paths.reserve(pathCount);
+		m_lightVertices.reserve(pathCount);
+		m_lightVertices.clear();
 		for(int i = 0; i<pathCount; ++i) {
 			Float time = i*1000;
 			//Log(EInfo, "%i", i);
@@ -83,9 +95,32 @@ public:
 			//Log(EInfo, "path initialized");
 			//m_config.dump()
 			emitterPath->randomWalk(scene, scene->getSampler(), m_config.maxDepth, m_config.rrDepth, EImportance, m_pool );
+
+			for(int vertexIdx = 0; vertexIdx < emitterPath->vertexCount(); vertexIdx++) {
+				PathVertexPtr vertex = emitterPath->vertex(vertexIdx);
+				// TODO: store all information in the
+				// PathVertex struct
+				// vertex->data[EdVCMData] = ...
+				// vertex->data[EdVCData] = ...
+				// vertex->data[EdVMData] = ...
+				m_lightVertices.push_back(vertex);
+			}
+
+
+
 			scene->getSampler()->advance();
 			//Log(EInfo, "walk done");
 			paths.push_back(emitterPath);
+		}
+
+
+		/////////////////////////////////////////////////////////////////////////
+		// BUILD SEARCH STRUCT
+		/////////////////////////////////////////////////////////////////////////
+
+		m_tree.reserve(pathCount);
+		for(int i=0; i<m_lightVertices.size(); i++) {
+		    m_tree.push_back(m_lightVertices[i]);
 		}
 
 		for(int i = 0; i<pathCount; ++i){
@@ -101,6 +136,8 @@ public:
 	MTS_DECLARE_CLASS()
 private:
 	ref<ParallelProcess> m_process;
+	vector<PathVertexPtr> m_lightVertices;
+	VCMKDTree m_tree;
 	VCMConfiguration m_config;
 	MemoryPool m_pool;
 };
